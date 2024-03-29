d5e6a2d51a88b841b5a9fe81356533e100509e93
ushandilusha <ushandilusha10@gmail.com>
2024-03-29 22:45:02 +0530

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -11,42 +11,23 @@ import CommunityAddPage from "./pages/communityPage/CommuityAdd";
 import CommunityUpdatePage from "./pages/communityPage/CommuityUpdate";
 
 function App() {
-    return ( <
-        div className = "app" >
-        <
-        BrowserRouter >
-        <
-        Routes > { /* add your pages path and link it */ } <
-        Route path = "/admin"
-        element = { < AdminDashboard / > }
-        /> <
-        Route path = "/community-view"
-        element = { < CommunityView / > }
-        /> <
-        Route path = "/community-add"
-        element = { < CommunityAddPage / > }
-        /> <
-        Route path = "/community-update/:id"
-        element = { < CommunityUpdatePage / > }
-        /> <
-        Route path = "/"
-        element = { < Home / > }
-        /> <
-        Route path = "/addcourse"
-        element = { < AddCourse / > }
-        /> <
-        Route path = "/all"
-        element = { < ViewCourses / > }
-        /> <
-        Route path = "/update/:id"
-        element = { < UpdateCourse / > }
-        /> <
-        Route path = "/user"
-        element = { < UserDashboard / > }
-        /> <
-        /Routes> <
-        /BrowserRouter> <
-        /div>
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Routes>
+                    {/* add your pages path and link it */}
+                    <Route path="/admin" element={<AdminDashboard />} />
+                    <Route path="/community-view" element={<CommunityView />} />
+                    <Route path="/community-add" element={<CommunityAddPage />} />
+                    <Route path="/community-update/:id" element={<CommunityUpdatePage />} />
+                    <Route path="/" element={<Home />} />
+                    <Route path="/addcourse" element={<AddCourse />} />
+                    <Route path="/all" element={<ViewCourses />} />
+                    <Route path="/update/:id" element={<UpdateCourse />} />
+                    <Route path="/user" element={<UserDashboard />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
 }
 
