90e831ae7a6763de5eab80fde2cd9f2df453e1f9
Ishara Madushan <IT21183904@my.sliit.lk>
2024-03-30 01:31:42 +0530

    done

--- src/pages/Course/AddCourse.jsx
+++ src/pages/Course/AddCourse.jsx
@@ -1,16 +1,13 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useState } from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import swal from 'sweetalert';
+
 
 function AddCourse() {
-
-
-
-
-
-
-
-//Add
-const [category, setCategory] = useState("");
+  //Add
+  const [category, setCategory] = useState("");
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [resources, setResources] = useState("");
@@ -18,17 +15,13 @@ const [category, setCategory] = useState("");
   const [chapters, setChapters] = useState("");
   const [price, setPrice] = useState("");
 
-
-
-  function onChangeFile(e){
+  function onChangeFile(e) {
     setImage(e.target.files[0]);
   }
-  
-  
-  
-  function changeOnClick(e){
-    e.preventDefault();   
-  
+
+  function changeOnClick(e) {
+    e.preventDefault();
+
     const formData = new FormData();
     formData.append("category", category);
     formData.append("title", title);
@@ -37,114 +30,147 @@ const [category, setCategory] = useState("");
     formData.append("chapters", chapters);
     formData.append("price", price);
     formData.append("image", image);
-  
-    axios.post("http://localhost:8070/api/courses", formData, {
-      headers: {"Content-Type": "multipart/form-data"},
-    }).then(() => {
-      alert("Course details added");
-      setCategory("");
-      setTitle("");
-      setDescription("");
-      setResources("");
-      setChapters("");
-      setPrice("");
-      window.location.href = "/all";
-    }).catch((err) => {
-      alert(err);
-    });
+
+    axios
+      .post("http://localhost:8070/api/courses", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+
+      .then(() => {
+        swal("Category Added Successfully", " ", "success")
+        setCategory("");
+        setTitle("");
+        setDescription("");
+        setResources("");
+        setChapters("");
+        setPrice("");
+        window.location.href = "/all";
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
-  
 
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-10 sm:px-4 sm:py-15 lg:px-8 flex items-center">
-      <div className="w-2/3 p-8">
-        <h1 className="text-2xl lg:text-4xl font-bold py-6">Add Course</h1>
-        <div className="shadow-lg border-2 rounded-lg p-8">
-          <form   encType="multipart/form-data"  onSubmit={changeOnClick} className="space-y-8">
-            <div>
-              <label className="block text-lg lg:text-xl mb-2">Category:</label>
-              <input
-                type="text"
-                name="category"
-                value={category}
-                onChange={(e)=>{setCategory(e.target.value)}}
-                className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/>
-            </div>
-            <div>
-              <label className="block text-lg lg:text-xl mb-2">Title:</label>
-              <input
-                type="text"
-                name="title"
-                value={title}
-                onChange={(e)=>{setTitle(e.target.value)}}
-                className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/> 
-            </div>
-            <div>
-              <label className="block text-lg lg:text-xl mb-2">Description:</label>
-              <textarea
-                name="description"
-                value={description}
-                onChange={(e)=>{setDescription(e.target.value)}}
-                className="w-full h-32 border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/> 
-            </div>
-            <div>
-              <label className="block text-lg lg:text-xl mb-2">Resources:</label>
-              <input
-                type="text"
-                name="resources"
-                value={resources}
-                onChange={(e)=>{setResources(e.target.value)}}
-                className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500 "/>
-            </div>
-
-            <div className="mb-4">
-              <label className="block text-lg lg:text-xl mb-2">Thumbnail:</label>
-              <div className="flex items-center">
+    <>
+      <Header />
+      <div className="flex justify-center">
+        <div className="w-full max-w-xl mt-10">
+          <h1 className="mb-4 text-3xl font-bold">Add Course</h1>
+          <div className="shadow-lg border-2 rounded-lg p-8">
+            <form
+              encType="multipart/form-data"
+              onSubmit={changeOnClick}
+              className="space-y-8"
+            >
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">
+                  Category:
+                </label>
                 <input
-                  type="file"
-                  accept="image/*"
-                  name="image"
-                  onChange={onChangeFile}
-                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/>
-                
+                  type="text"
+                  name="category"
+                  value={category}
+                  onChange={(e) => {
+                    setCategory(e.target.value);
+                  }}
+                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                />
               </div>
-            
-            </div>    
-            <div>
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">Title:</label>
+                <input
+                  type="text"
+                  name="title"
+                  value={title}
+                  onChange={(e) => {
+                    setTitle(e.target.value);
+                  }}
+                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">
+                  Description:
+                </label>
+                <textarea
+                  name="description"
+                  value={description}
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                  }}
+                  className="w-full h-32 border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">
+                  Resources:
+                </label>
+                <input
+                  type="text"
+                  name="resources"
+                  value={resources}
+                  onChange={(e) => {
+                    setResources(e.target.value);
+                  }}
+                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500 "
+                />
+              </div>
 
-              
-              <label className="block text-lg lg:text-xl mb-2">Chapters:</label>
-              <input
-                type="text"
-                name="chapters"
-                value={chapters}
-                onChange={(e)=>{setChapters(e.target.value)}}
-                className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/>
-                 
-            </div>
-            <div>
-              <label className="block text-lg lg:text-xl mb-2">Price:</label>
-              <input
-                type="text"
-                name="price"
-                value={price}
-                onChange={(e)=>{setPrice(e.target.value)}}
-                className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"/>
-                
-
-            </div>
-            <div className="mt-10">
+              <div className="mb-4">
+                <label className="block text-lg lg:text-xl mb-2">
+                  Thumbnail:
+                </label>
+                <div className="flex items-center">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    name="image"
+                    onChange={onChangeFile}
+                    className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                  />
+                </div>
+              </div>
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">
+                  Chapters:
+                </label>
+                <input
+                  type="text"
+                  name="chapters"
+                  value={chapters}
+                  onChange={(e) => {
+                    setChapters(e.target.value);
+                  }}
+                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div>
+                <label className="block text-lg lg:text-xl mb-2">Price:</label>
+                <input
+                  type="text"
+                  name="price"
+                  value={price}
+                  onChange={(e) => {
+                    setPrice(e.target.value);
+                  }}
+                  className="w-full border rounded-lg p-2 focus:ring focus:ring-blue-500 focus:border-blue-500"
+                />
+              </div>
+              <div className="mt-10">
               <button
                 type="submit"
                 className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
             >
                Submit
-            </button>       
-            </div>
-          </form>
+            </button> 
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Course/ViewCourses.jsx
+++ src/pages/Course/ViewCourses.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
 
 //....Delete Course....//
 
@@ -14,9 +14,12 @@ const DeleteCourse = ({ courseId, onDelete }) => {
     try {
       await axios.delete(`http://localhost:8070/api/courses/${courseId}`);
       onDelete(courseId); // Notify parent component about the deletion
-      console.log('Course deleted successfully');
+      console.log("Course deleted successfully");
     } catch (error) {
-      setDeleteError(error.response.data.message || 'An error occurred while deleting the course');
+      setDeleteError(
+        error.response.data.message ||
+          "An error occurred while deleting the course"
+      );
     } finally {
       setIsDeleting(false);
     }
@@ -26,7 +29,7 @@ const DeleteCourse = ({ courseId, onDelete }) => {
     <div>
       {deleteError && <p>Error: {deleteError}</p>}
       <button onClick={handleDelete} disabled={isDeleting}>
-        {isDeleting ? 'Deleting...' : 'Delete'}
+        {isDeleting ? "Deleting..." : "Delete"}
       </button>
     </div>
   );
@@ -37,31 +40,30 @@ const DeleteCourse = ({ courseId, onDelete }) => {
 function ViewCourses() {
   const [courses, setCourses] = useState([]);
 
-   
   useEffect(() => {
-     fetchData();
+    fetchData();
   }, []);
-  
+
   const fetchData = async () => {
     try {
-        const response = await axios.get("http://localhost:8070/api/courses/getall");
-        if (response && response.data) {
-            setCourses(response.data);
-        } else {
-            console.error("Empty response or missing data:", response);
-        }
+      const response = await axios.get(
+        "http://localhost:8070/api/courses/getall"
+      );
+      if (response && response.data) {
+        setCourses(response.data);
+      } else {
+        console.error("Empty response or missing data:", response);
+      }
     } catch (error) {
-        console.error("Error fetching data:", error);
+      console.error("Error fetching data:", error);
     }
-};
-
-
-  
-
-  const handleDelete = (deletedCourseId) => {
-    setCourses(prevCourses => prevCourses.filter(course => course._id !== deletedCourseId));
   };
 
+  const handleDelete = (deletedCourseId) => {
+    setCourses((prevCourses) =>
+      prevCourses.filter((course) => course._id !== deletedCourseId)
+    );
+  };
 
   const handleViewOrder = (_id) => {
     // Construct the URL with the orderId
@@ -77,44 +79,79 @@ function ViewCourses() {
   };
 
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-10 sm:px-4 sm:py-15 lg:px-8">
-      <h1 className="text-3xl lg:text-4xl font-bold mb-8">All Courses</h1>
-      <button onClick={addNavigation} style={{marginLeft:"1100px"}} type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200">Add Course</button>
-      <br></br> <br></br>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      {courses.length > 0 ? (
-          courses.map((course) => {
-            // Handle potential null or undefined values
-            const base64String = course.image && course.image.data ? btoa(
-              String.fromCharCode(...new Uint8Array(course.image.data.data))
-            ) : '';
-            return (
-              <div key={course._id} className="relative bg-white rounded-lg shadow-md overflow-hidden">
-                <div className="absolute top-0 right-0 z-10">
-                  <button onClick={() => { console.log(course._id); handleViewOrder(course._id); }}  className ="text-blue-500 hover:text-blue-600 mr-2">
-                    Edit 
-                  </button>
-                  <button className="text-red-500 hover:text-red-600">
-                    <DeleteCourse courseId={course._id} onDelete={handleDelete}/>
-                  </button>
-                </div>
-                <img src={`data:image/png;base64,${base64String}`} alt="Thumbnail"  className="w-full h-40 object-cover" />
-                <div className="p-6">
-                  <h2 className="text-xl font-semibold mb-2">{course.title}</h2>
-                  <p className="text-gray-600 mb-4">{course.description}</p>
-                  <div className="flex justify-between items-center">
-                    <p className="text-gray-700">Category: {course.category}</p>
-                    <p className="text-blue-500 font-bold">{course.price}</p>
+    <>
+      <Header />
+      <div className="mx-auto max-w-screen-xl px-4 py-10 sm:px-4 sm:py-15 lg:px-8">
+        <h1 className="text-3xl lg:text-4xl font-bold mb-8">All Courses</h1>
+        <button
+          onClick={addNavigation}
+          style={{ marginLeft: "1100px" }}
+          type="submit"
+          className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-blue-200"
+        >
+          Add Course
+        </button>
+        <br></br> <br></br>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {courses.length > 0 ? (
+            courses.map((course) => {
+              // Handle potential null or undefined values
+              const base64String =
+                course.image && course.image.data
+                  ? btoa(
+                      String.fromCharCode(
+                        ...new Uint8Array(course.image.data.data)
+                      )
+                    )
+                  : "";
+              return (
+                <div
+                  key={course._id}
+                  className="relative bg-white rounded-lg shadow-md overflow-hidden"
+                >
+                  <img
+                    src={`data:image/png;base64,${base64String}`}
+                    alt="Thumbnail"
+                    className="w-full h-40 object-cover"
+                  />
+                  <div className="p-6">
+                    <h2 className="text-xl font-semibold mb-2">
+                      {course.title}
+                    </h2>
+                    <p className="text-gray-600 mb-4">{course.description}</p>
+                    <div className="flex justify-between items-center">
+                      <p className="text-gray-700">
+                        Category: {course.category}
+                      </p>
+                      <p className="text-blue-500 font-bold">{course.price}</p>
+                    </div>
+                    <div className="mt-6 top-0 right-0 z-10">
+                      <button
+                        onClick={() => {
+                          console.log(course._id);
+                          handleViewOrder(course._id);
+                        }}
+                        className="bg-blue-500 text-white py-1 px-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
+                      >
+                        Edit
+                      </button>
+                      <button className="bg-red-500 text-white py-1 ml-3 px-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring focus:ring-blue-200">
+                        <DeleteCourse
+                          courseId={course._id}
+                          onDelete={handleDelete}
+                        />
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })
-        ) : (
-          <p className="text-lg text-center">No courses available</p>
-        )}
+              );
+            })
+          ) : (
+            <p className="text-lg text-center">No courses available</p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default ViewCourses;
