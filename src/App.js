aa14e46549078bc0fce83d18eda69161f1c561dc
ushandilusha <ushandilusha10@gmail.com>
2024-03-29 22:41:38 +0530

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import AdminDashboard from "./pages/adminDashboard/AdminDashboard";
-import CommunityPage from "./pages/communityPage/CommunityPage";
+import CommunityView from "./pages/communityPage/CommunitiesView";
 import UserDashboard from "./pages/userDashboard/userDashboard";
-
+import CommunityAddPage from "./pages/communityPage/CommuityAdd";
+import CommunityUpdatePage from "./pages/communityPage/CommuityUpdate";
 
 function App() {
     return (
@@ -13,7 +14,9 @@ function App() {
                 <Routes>
                     {/* add your pages path and link it */}
                     <Route path="/admin" element={<AdminDashboard />} />
-                    <Route path="/community" element={<CommunityPage />} />
+                    <Route path="/community-view" element={<CommunityView />} />
+                    <Route path="/community-add" element={<CommunityAddPage />} />
+                    <Route path="/community-update/:id" element={<CommunityUpdatePage />} />
                     <Route path="/" element={<Home />} />
                     <Route path="/user" element={<UserDashboard />} />
                 </Routes>
